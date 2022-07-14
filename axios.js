a43f1be3ae37528c83b67255e23a17a1ff5651ea
luchoca <luchocastro.3@gmail.com>
2022-07-14 13:37:43 -0300

    Se crea funcion POST

--- axios.js
+++ axios.js
@@ -39,3 +39,46 @@ const getAll = async () => {
 };
 
 d.addEventListener("DOMContentLoaded", getAll);
+
+//en el submit/ enviar
+// POST Y DELETE
+//si el id (nombre="id") viene vacio lo va a poner (post)
+//y si viene con informacion lo eliminar (DELETE)
+d.addEventListener("submit", async (e) => {
+  if (e.target === $form) {
+    //esto se ejecuta si el objeto que ejecuta el evento sea el Formulario
+    e.preventDefault(); // por eso cancelamos el default para que espere las ordenes de JS
+
+    if (!e.target.id.value) {
+      //si dentro del input no hay valor hacemos un POST
+      //Create - POST
+      try {
+        let options = {
+            method: "POST",
+            headers: {
+              "Content-type": "application/json; charset=utf-8",
+            },
+            data: JSON.stringify({
+              //lo pasa a formato json
+              //lo que para fetch es "body" aca en axios es "data"
+              nombre: e.target.nombre.value, // son las cajas de texto (input) del form
+              constelacion: e.target.constelacion.value, // son las cajas de texto (input) del form
+            }),
+          },
+          res = await axios("http://localhost:4444/santos", options),
+          json = await res.data; //aca en fetch seria json , pero en axios es data
+
+        // AXIOS NO PRECISA MANIPULAR EL ERROR POR QUE LO MANDA DIRECTO AL CATCH
+
+        location.reload(); // recarga pagina para nueva isercion
+      } catch (err) {
+        let message = err.statusText || "OH NO!!! ocurrio un error";
+        $form.insertAdjacentHTML(
+          // ponemos el mensaje de error abajo del form
+          "afterend",
+          `<p><b>Error ${err.status}: ${message}</b></p>`
+        );
+      }
+    }
+  }
+});
